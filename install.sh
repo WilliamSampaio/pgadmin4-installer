9d2803280f9fa9c9bd73a89fe98d79925cfed14e
William B M Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-11-27 15:13:14 -0400

    add uinstall

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+sudo mkdir /var/lib/pgadmin
+sudo mkdir /var/log/pgadmin
+sudo chown $USER /var/lib/pgadmin
+sudo chown $USER /var/log/pgadmin
 python -m venv $HOME/.pgadmin4
 source $HOME/.pgadmin4/bin/activate
 pip install --upgrade pip
 pip install pgadmin4
 sudo curl https://raw.githubusercontent.com/WilliamSampaio/pgadmin4-installer/master/pgadmin4 --output /usr/bin/pgadmin4
 sudo chmod +x /usr/bin/pgadmin4
-$HOME/.pgadmin4/bin/pgadmin4
+pgadmin4
