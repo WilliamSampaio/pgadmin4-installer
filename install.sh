2a821b28fa3519fc4814489454a0921611f0160b
William B M Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-11-27 14:41:38 -0400

    update

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ pip install --upgrade pip
 pip install pgadmin4
 sudo curl https://raw.githubusercontent.com/WilliamSampaio/pgadmin4-installer/master/pgadmin4 --output /usr/bin/pgadmin4
 sudo chmod +x /usr/bin/pgadmin4
-pgadmin4
+$HOME/.pgadmin4/bin/pgadmin4
